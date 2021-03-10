5707cf45e8391fb0c62e4cda3b1983cf98b67e19
ChubbyPanda321 <79952651+ChubbyPanda321@users.noreply.github.com>
2021-03-10 16:48:30 +0800

    Update r_rate.py

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -4,7 +4,7 @@ r = float(input('r_rate='))
 n = 84
 #caculate the number of infected individuals and remove the decimal
 for i in range(1,6):
-    n = n*r
+    n = n*(r+1)
 n = int(n)
 #print the result
 print(str(n)+' individuals are infected after 5 generations with r='+str(r))
