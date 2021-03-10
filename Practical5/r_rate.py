40047033d2548338ec374baba25cb7cdeabc69f5
ChubbyPanda321 <79952651+ChubbyPanda321@users.noreply.github.com>
2021-03-10 13:09:43 +0800

    Create r_rate.py

---
+++ Practical5/r_rate.py
@@ -0,0 +1,10 @@
+print('I want to make a modifiable version that you can change the r_rate easier :-P')
+#define the variables
+r = float(input('r_rate='))
+n = 84
+#caculate the number of infected individuals and remove the decimal
+for i in range(1,6):
+    n = n*r
+n = int(n)
+#print the result
+print(str(n)+' individuals are infected after 5 generations with r='+str(r))
