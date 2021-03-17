e1e659a9a0f1e89224bbaaeb5ee046a75e47942c
ChubbyPanda321 <anxuan.20@intl.zju.edu.cn>
2021-03-17 09:58:25 +0800

    Teacher may prefer this version

--- Practical6/coronavirus_infection_rates.py
+++ Practical6/coronavirus_infection_rates.py
@@ -1,7 +1,8 @@
 #import matpoltlib
 import matplotlib.pyplot as plt
 #define a function so it's easier to use later
-def mkplot_infection_rate(**case):  
+def mkplot_infection_rate(**case): 
+    print(case) 
     plt.pie(
         case.values(),
         labels = case.keys(),
@@ -19,12 +20,10 @@ def mkplot_infection_rate(**case):
     plt.show()
     return case
 #use the function to create to plot
-cases = mkplot_infection_rate(
+mkplot_infection_rate(
     USA = 29862124,
     India = 11285561,
     Brazil = 11205972,
     Russia = 4360823,
     UK = 4234924
     )
-#print the dict of the infected cases
-print(cases)
