51fedab3a3cba2df2028ad089498a1418096f871
ChubbyPanda321 <anxuan.20@intl.zju.edu.cn>
2021-03-31 18:30:41 +0800

    maybe better

--- Practical8/get_unknown_function.py
+++ Practical8/get_unknown_function.py
@@ -2,16 +2,15 @@ import os, re
 os.chdir('D:\ZJU\IBI\python\IBI1_2020-21\Practical8')
 with open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa') as allcdna:
     with open('unknown_function.fa','w') as output:
-        seqlinenum = 0
+        seq = ''
         for line in allcdna:
             if line.startswith('>'):
                 getnextseq = False
                 if 'unknown function' in line:
-                    if seqlinenum != 0:
-                        output.write(f'{name:20}'+str(len(seq)-seqlinenum)+'\n'+seq)
+                    if len(seq) != 0:
+                        output.write(f'{name:20}'+str(len(seq))+'\n'+seq+'\n')
                     name = re.search(r'(?<=\bgene:)(\w|-)+', line).group()
-                    seq, seqlinenum, getnextseq = '', 0, True
+                    seq,  getnextseq = '', True
             elif getnextseq:
-                seqlinenum += 1
-                seq += line
-        output.write(f'{name:20}'+str(len(seq)-seqlinenum)+'\n'+seq)
+                seq += line[:-1]
+        output.write(f'{name:20}'+str(len(seq))+'\n'+seq)
