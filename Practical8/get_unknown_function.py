fa1ada14be2a7e1d0e9294fd2b4a916e9f04ae75
ChubbyPanda321 <anxuan.20@intl.zju.edu.cn>
2021-03-31 17:56:43 +0800

    fix the finial seqlen

--- Practical8/get_unknown_function.py
+++ Practical8/get_unknown_function.py
@@ -1,22 +1,17 @@
-import os
-import re
+import os, re
 os.chdir('D:\ZJU\IBI\python\IBI1_2020-21\Practical8')
-allcdna = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
-output = open('unknown_function.fa','w')
-seqlinenum = 0
-for line in allcdna:
-    if line.startswith('>'):
-        getnextseq = True
-        if 'unknown function' in line:
-            if seqlinenum != 0:
-                length = len(seq) - seqlinenum
-                output.write(f'{name:20}'+str(length)+'\n'+seq)
-            name = re.search(r'(?<=\bgene:)\w+', line).group()
-            seq, seqlinenum = '', 0
-            getnextseq = False
-    elif not getnextseq:
-        seqlinenum += 1
-        seq += line
-output.write(f'{name:20}'+str(length)+'\n'+seq)
-allcdna.close()
-output.close()
+with open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa') as allcdna:
+    with open('unknown_function.fa','w') as output:
+        seqlinenum = 0
+        for line in allcdna:
+            if line.startswith('>'):
+                getnextseq = False
+                if 'unknown function' in line:
+                    if seqlinenum != 0:
+                        output.write(f'{name:20}'+str(len(seq)-seqlinenum)+'\n'+seq)
+                    name = re.search(r'(?<=\bgene:)\w+', line).group()
+                    seq, seqlinenum, getnextseq = '', 0, True
+            elif getnextseq:
+                seqlinenum += 1
+                seq += line
+        output.write(f'{name:20}'+str(len(seq)-seqlinenum)+'\n'+seq)
