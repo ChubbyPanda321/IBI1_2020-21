84bb132b599e8ca0eae4d12bfb0e8c075f4d4d5c
ChubbyPanda321 <anxuan.20@intl.zju.edu.cn>
2021-04-13 10:21:10 +0800

    task 1

---
+++ Practical10/Reverse_complement_calculator.py
@@ -0,0 +1,11 @@
+def rev_compl(seq:str) -> str:
+    '''
+    This is a function that convert a DNA sequence to its reverse complement sequence.
+    Upper case, lower case or a mix of both are Ok as input.
+    The output will be unified as upper case.
+    '''
+    return seq.upper().replace('A', 't').replace('T', 'a').replace('C', 'g').replace('G', 'c').upper()[::-1]
+
+seq = 'ACcCaTtG'
+rev_compl_seq = rev_compl(seq)
+print(rev_compl_seq)
