9ebbf456f6cd9a6fb43a8a1a53c443c502f7444e
ChubbyPanda321 <anxuan.20@intl.zju.edu.cn>
2021-04-21 09:23:02 +0800

    add name

--- Practical11/Alignment.py
+++ Practical11/Alignment.py
@@ -12,7 +12,7 @@ for row_num in range(len(index)):
 
 # read the sequences
 def readfa_oneseq(filehandle):
-    return filehandle.readline().rstrip(), filehandle.readline().rstrip()
+    return filehandle.readline()[1:-1], filehandle.readline().rstrip()
 with open('RandomSeq.fa') as R:
     rand_name, rand_seq = readfa_oneseq(R)
 with open('SOD2_human.fa') as H:
@@ -21,7 +21,7 @@ with open('SOD2_mouse.fa') as M:
     mou_name, mou_seq = readfa_oneseq(M)
 
 # compare
-def pairwisw_global_alignment(seq1,seq2):
+def pairwisw_global_alignment(seq1,seq2,seq1_name,seq2_name):
     score = 0
     count = 0
     print_alignment = ''
@@ -29,14 +29,14 @@ def pairwisw_global_alignment(seq1,seq2):
         score += BLOSUM62_dict[(aa1,aa2)]
         if aa1 == aa2:
             count += 1
-            print_alignment += '|'
+            print_alignment += aa1
         else:
-            print_alignment += ' '
-    print('Sequence1:',seq1)
-    print('Alignment:',print_alignment)
-    print('Sequence2:',seq2)
-    print(f'BLOSUM62 Score: {score:<10} Score per aa: {score/len(seq1):<10.2f} Percentage of identity aa: {count*100/len(seq1):.2f}% ')
+            print_alignment += '*'
+    print(seq1_name+':\n'+seq1)
+    print('Alignment:\n'+print_alignment)
+    print(seq2_name+':\n'+seq2)
+    print(f'BLOSUM62 Score: {score:<10} Score per aa: {score/len(seq1):<10.2f} Percentage of identity aa: {count*100/len(seq1):.2f}% '+'\n')
         
-pairwisw_global_alignment(hum_seq, mou_seq)
-pairwisw_global_alignment(hum_seq, rand_seq)
-pairwisw_global_alignment(mou_seq, rand_seq)
+pairwisw_global_alignment(hum_seq, mou_seq, hum_name, mou_name)
+pairwisw_global_alignment(hum_seq, rand_seq, hum_name, rand_name)
+pairwisw_global_alignment(mou_seq, rand_seq, mou_name, rand_name)
